cfb7785d8b3406e59940cc8dab9d45e361183cb3
alonlerner <al663@njit.edu>
2020-09-12 00:09:05 -0400

    Updates

---
+++ precache-manifest.a58b37037ef54c4bb352f6d2eb4f48c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5cd4309e3b7c7916c63da45df2948b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "537a12670028722f8fa2",
+    "url": "/Portfolio/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "3b4c51472d57106df618",
+    "url": "/Portfolio/static/css/main.87cef56f.chunk.css"
+  },
+  {
+    "revision": "537a12670028722f8fa2",
+    "url": "/Portfolio/static/js/2.9fcbd6ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.9fcbd6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4c51472d57106df618",
+    "url": "/Portfolio/static/js/main.3aaaec9d.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "30b150cd489202db131009ac9540cec0",
+    "url": "/Portfolio/static/media/GitHub.30b150cd.png"
+  },
+  {
+    "revision": "c4f67848c3bea5165ff7ed820ba8b964",
+    "url": "/Portfolio/static/media/HangMan.c4f67848.mp4"
+  },
+  {
+    "revision": "8f7dd86aa1ccc1b3ceacfb1ad82f1dfa",
+    "url": "/Portfolio/static/media/LinkedIn.8f7dd86a.png"
+  },
+  {
+    "revision": "1b934228ead4c6138e7a7acd219c28ba",
+    "url": "/Portfolio/static/media/SongsRecGame.1b934228.mp4"
+  }
+]);
